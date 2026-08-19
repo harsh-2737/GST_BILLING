62e84831f9efdcedfad716c37ce3cdcad734001b
Harsh <pharsh5727@gmail.com>
2026-08-19 08:55:51 +0530

    first commit

---
+++ backend/controllers/UserController.js
@@ -0,0 +1,23 @@
+const User = require("../models/User");
+
+const createUser = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+
+        const user = await User.create({
+            name,
+            email,
+            password
+        });
+
+        res.status(201).json(user);
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
+};
+
+module.exports = {
+    createUser
+};
---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+app.use("/api/users", userRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+    app.get("/", (req, res) => {
+    res.send("Server is running");
+});
+});
